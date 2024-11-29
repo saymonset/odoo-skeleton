9ab179a902fd8bb0c5d3693ed5c00922c5de807a
Simon <saymon_set@hotmail.com>
2024-11-29 09:25:13 -0400

    kanbas con barra. menu para crear. tarjetas dobladas. chapter-12->05

--- docker-instalacion-17/v17/extra-addons/my_hostel/__manifest__.py
+++ docker-instalacion-17/v17/extra-addons/my_hostel/__manifest__.py
@@ -1,20 +1,31 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "My Hostel",  # Module title
-    'summary': "Manage Hostel easily",  # Module subtitle phrase
-    'description': """
-Long description
-    """,  # Supports reStructuredText(RST) format
+    "name": "Hostel Management",  # Module title
+    "summary": "Manage Hostel easily",  # Module subtitle phrase
+    "description": """
+Manage Hostel
+==============
+Efficiently manage the entire residential facility in the school
+    """,  # Supports reStructuredText(RST) format (description is Deprecated)
     "version": "17.0.1.0.0",
     "author": "Serpent Consulting Services Pvt. Ltd.",
-    "category": "Uncategorized",
+    "category": "Tools",
     "website": "http://www.serpentcs.com",
-    "depends": ['mail'],
     "license": "AGPL-3",
-    'data': [
-        'security/groups.xml',
-        'security/ir.model.access.csv',
-        'views/hostel_room_category_view.xml',
-        'views/hostel_room.xml',
+    "depends": ["base"],
+    "data": [
+        "security/hostel_security.xml",
+        "security/ir.model.access.csv",
+        "data/room_stages.xml",
+        "views/hostel.xml",
+        "views/hostel_amenities.xml",
+        "views/hostel_room.xml",
+        "views/hostel_room_stages_views.xml",
+        "views/hostel_student.xml",
+        "views/hostel_categ.xml",
     ],
+    # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
+    "installable": True,
 }
--- docker-instalacion-17/v17/extra-addons/my_hostel/models/__init__.py
+++ docker-instalacion-17/v17/extra-addons/my_hostel/models/__init__.py
@@ -1,3 +1,5 @@
-# -*- coding: utf-8 -*-
 from . import hostel
-from . import hostel_room_category
+from . import hostel_room
+from . import hostel_amenities
+from . import hostel_student
+from . import hostel_categ
