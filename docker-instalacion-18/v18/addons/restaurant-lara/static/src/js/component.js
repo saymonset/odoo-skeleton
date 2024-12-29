f8cd9e1d054e547de5187fe7de703e7141edec9b
Simon <saymon_set@hotmail.com>
2024-12-29 09:29:07 -0400

    OWL colocando lista dinamica con botones next prev odoo 18

--- docker-instalacion-18/v18/addons/restaurant-lara/static/src/js/component.js
+++ docker-instalacion-18/v18/addons/restaurant-lara/static/src/js/component.js
@@ -1,5 +1,5 @@
 /** @odoo-module **/
-import { Component, mount, xml, whenReady } from "@odoo/owl";
+import { Component, mount, xml, whenReady, useState } from "@odoo/owl";
 odoo.define('restaurant-lara.component', [], function (require) {
     "use strict";
 
@@ -8,14 +8,43 @@ odoo.define('restaurant-lara.component', [], function (require) {
     class MyComponent extends Component {
         static template = xml`
             <div class="bg-info text-white text-center p-3">
-                <b> Welcome To RESTAURANT-LARAXX</b>
-                <i class="fa fa-close p-1 float-end"
+                   
+                    <i class="fa fa-arrow-left p-1"
                     style="cursor: pointer;"
-                    t-on-click="onRemove"> </i>
+                    t-on-click="onPrevious"> </i>
+                   
+                    <b t-esc="messageList[Math.abs(state.currentIndex%4)]"/>
+                   
+                    <i class="fa fa-arrow-right p-1"
+                        style="cursor: pointer;"
+                        t-on-click="onNext"> </i>
+                  
+                  
+                        <i class="fa fa-close p-1 float-end"
+                        style="cursor: pointer;"
+                        t-on-click="onRemove"> </i>
             </div>`
+            setup() {
+                this.messageList = [
+                    'Hello World',
+                    'Welcome to Odoo',
+                    'Odoo is awesome',
+                    'You are awesome too'
+                ];
+                this.state = useState({ currentIndex: 0 });
+            }
             onRemove(ev) {
                 ev.target.closest("div").remove(); // Elimina el contenedor del componente
             }
+            onNext(ev) {
+                this.state.currentIndex++;
+            }
+            onPrevious(ev) {
+                this.state.currentIndex--;
+            }
+
+
+
     }
     whenReady().then(() => {
         mount(MyComponent, document.body);
