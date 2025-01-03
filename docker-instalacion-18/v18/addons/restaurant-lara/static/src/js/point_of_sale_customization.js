00d02b18d441ceb17675c91593cb7be01a97538c
Simon <saymon_set@hotmail.com>
2025-01-03 08:16:31 -0400

    Ya el owl contexto funciona

---
+++ docker-instalacion-18/v18/addons/restaurant-lara/static/src/js/point_of_sale_customization.js
@@ -0,0 +1,75 @@
+/** @odoo-module **/
+import { Component } from "@odoo/owl";
+import { ProductScreen } from  "@point_of_sale/app/screens/product_screen/product_screen";
+import { usePos } from "@point_of_sale/app/store/pos_hook";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { _t } from "@web/core/l10n/translation";
+import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import { patch } from "@web/core/utils/patch";
+
+// Definir el botón de descuento
+export class PosDiscountButton extends Component {
+    static template = "PosDiscountButton";
+
+    setup() {
+       this.pos = usePos();
+    }
+
+    async onClick() {
+        alert('Eres una bestia!')
+        // const order = this.pos.get_order();
+        // if (order.selected_orderline) {
+        //     order.selecbted_orderline.set_discount(5);
+        // }
+    }
+}
+
+
+
+// Parchear ControlButtons para agregar el botón de descuento
+patch(ControlButtons.prototype, {
+    async onClick() {
+         // Create an instance of PosDiscountButton
+        //  const discountButton = new PosDiscountButton();
+        //  await discountButton.onClick(); // Call the onClick method of the discount button
+
+         const order = this.pos.get_order();
+         const {selectOrderLine} = this.pos;
+         const pos = this.pos;
+         const { company , user } = this.pos;
+         const { name:nameUser } = user;
+         const orderLines = order.get_orderlines();
+         console.log('-------------0--------------')
+         
+         console.log(selectOrderLine)
+         console.log('-------------1--------------')
+         console.log(pos)
+         console.log('------------1.2--------------------')
+         orderLines.forEach(line => {
+            //line.set_unit_price(line.product_id.price_for_employee || line.product_id.lst_price); // Update the price
+            line.set_unit_price(line.product_id.price_for_employee || line.product_id.lst_price); // Update the price
+        });
+       
+        if (order.selected_orderline) {
+            console.log('-------------2--------------')
+            order.selected_orderline.set_discount(5);
+        }
+        console.log('-------------3--------------')
+    
+      
+    },
+});
+
+// Parchear ControlButtons para agregar el botón de descuento
+
+
+ 
+patch(ControlButtons.prototype, {
+    async onClickPopup() {
+        this.dialog.add(AlertDialog, {
+            title: _t("Custom Alert saymon "),
+            body: _t("Choose the alert type Sonnyra"),
+        });
+      
+    },
+});
