f45d34e63c4a80f135e40561ade403cf9697d5f0
Simon <saymon_set@hotmail.com>
2025-01-16 14:44:49 -0400

    Enviar de hijo al padre

--- docker-instalacion-18/v18/addons/awesome_owl/static/src/counter/counter.js
+++ docker-instalacion-18/v18/addons/awesome_owl/static/src/counter/counter.js
@@ -4,6 +4,11 @@ import { Component,  useState } from "@odoo/owl";
 
 export class Counter extends Component {
     static template = "awesome_owl.counter";
+    static props = {
+        a: String,
+        b: String,
+        callback: Function,
+      };
     setup() {
         this.state = useState({ value: 0 });
     }
--- docker-instalacion-18/v18/addons/awesome_owl/static/src/playground.js
+++ docker-instalacion-18/v18/addons/awesome_owl/static/src/playground.js
@@ -6,5 +6,11 @@ import { Counter } from './counter/counter'
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter };
-    state = useState({ a: "fromparent" });
+    state = useState({ a: "fromparent", valueParent:0 });
+
+
+    callback(value) {
+        console.log('Hola desde el padre');
+        this.state.valueParent = value;
+      }
 }
