b8404c11a2692e19656620b8aa05450c3fd37065
Simon <saymon_set@hotmail.com>
2025-01-16 11:15:01 -0400

    OWL desde el padre pasar parametros al hijo

--- docker-instalacion-18/v18/addons/awesome_owl/static/src/playground.js
+++ docker-instalacion-18/v18/addons/awesome_owl/static/src/playground.js
@@ -6,4 +6,5 @@ import { Counter } from './counter/counter'
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter };
+    state = useState({ a: "fromparent" });
 }
