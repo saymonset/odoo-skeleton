e65ce64e1b6fdcc5bb11b2ee67fca0064a84c4a7
Simon <saymon_set@hotmail.com>
2025-01-16 11:07:54 -0400

    Contador hecho en u componenteseparadoi

---
+++ docker-instalacion-18/v18/addons/awesome_owl/static/src/playground.js
@@ -0,0 +1,9 @@
+/** @odoo-module **/
+
+import { Component,  useState } from "@odoo/owl";
+import { Counter } from './counter/counter'
+
+export class Playground extends Component {
+    static template = "awesome_owl.playground";
+    static components = { Counter };
+}
