9277c935e779a5ff4dcb7d7d215927bb1934e950
Simon <saymon_set@hotmail.com>
2025-01-27 18:16:43 -0400

    Agregar metodos que en las vistas son usados para cambiar el estado o validar que no se puede. we’ll learn how to create a function that may be used by our application’s user interface buttons or another piece of code.. Chapter05->01-02 ->odoo 18

--- docker-instalacion-18/v18/addons/my_hostel/models/hostel_room.py
+++ docker-instalacion-18/v18/addons/my_hostel/models/hostel_room.py
@@ -1,5 +1,5 @@
 from odoo import fields, models, api, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 
 
 class HostelRoom(models.Model):
@@ -33,6 +33,36 @@ class HostelRoom(models.Model):
         help="Students allocated per room")
     availability = fields.Float(compute="_compute_check_availability",
         store=True, string="Availability", help="Room availability in hostel")
+    room_rating = fields.Float('Hostel Average Rating', digits=(14, 4))
+    state = fields.Selection([
+        ('draft', 'Unavailable'),
+        ('available', 'Available'),
+        ('closed', 'Closed')],
+        'State', default="draft")
+    
+    @api.model
+    def is_allowed_transition(self, old_state, new_state):
+        allowed = [('draft', 'available'),
+                   ('available', 'closed'),
+                   ('closed', 'draft')]
+        return (old_state, new_state) in allowed
+
+    def change_state(self, new_state):
+        for room in self:
+            if room.is_allowed_transition(room.state, new_state):
+                room.state = new_state
+            else:
+                message = _('Moving from %s to %s is not allowed') % (room.state, new_state)
+                raise UserError(message)
+
+    def make_available(self):
+        self.change_state('available')
+
+    def make_closed(self):
+        self.change_state('closed')
+    
+    
+    
     _sql_constraints = [
        ("room_no_unique", "unique(room_no)", "Room number must be unique!")]
 
