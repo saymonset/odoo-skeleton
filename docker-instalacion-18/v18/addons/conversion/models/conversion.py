204144339726fbb398f176137d22b26404d33847
Simon <saymon_set@hotmail.com>
2025-01-29 15:20:09 -0400

    Conversiones ba

--- docker-instalacion-18/v18/addons/conversion/models/conversion.py
+++ docker-instalacion-18/v18/addons/conversion/models/conversion.py
@@ -6,7 +6,12 @@ from odoo import models, fields, api
 class conversion(models.Model):
     _name = 'conversion.conversion'
     _description = 'conversion'
+    _order = 'id desc'
 
-    name = fields.Char()
-    
+    name = fields.Char(string="Name")  # Campo tipo string
+    dateCurrency = fields.Date(string="Currency Date")  # Campo tipo fecha
+   # mount = fields.Float(string="Amount", digits=(16, 2))  # Campo tipo float con dos decimales
+    #currency_id = fields.Many2one('res.currency', string='Tipo de moneda')
+    #rent_amount = fields.Monetary('Rent Amount', help="Enter rent amount per month", string='Tipo de moneda') 
+
 
