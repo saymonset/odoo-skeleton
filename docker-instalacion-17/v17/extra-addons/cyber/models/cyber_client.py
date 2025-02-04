6428c9258b58764b443b355d307ed2e64b0b617f
Simon <saymon_set@hotmail.com>
2025-02-04 12:10:53 -0400

    Docker 18. Correrlo con DockerFile y no da error  de version odoo 18 como docker-compose

--- docker-instalacion-17/v17/extra-addons/cyber/models/cyber_client.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class Cliente(models.Model):
-    _name = 'cyber.cliente'
-    _description = 'Cliente del ciber'
-    _rec_name = 'nombre'
-
-    nombre = fields.Char(string='Nombre', required=True)
-    telefono = fields.Char(string='Teléfono', required=True)
