89eb122650110fb81f99c38c98e67674e7cdcf9c
saymonset <saymon_set@hotmail.com>
2025-02-06 16:11:15 +0000

    Caja de dialogo funcionando

--- docker/18.0/addons/conversion/static/src/app/screens/product_screen/payment_screen/custom_payment_screen/custom_payment_screen.js
+++ docker/18.0/addons/conversion/static/src/app/screens/product_screen/payment_screen/custom_payment_screen/custom_payment_screen.js
@@ -1,8 +1,11 @@
 /** @odoo-module **/
 import { _t } from "@web/core/l10n/translation";
-import { Component, useState, onRendered , onWillUpdateProps, onWillStart, useRef, useService } from "@odoo/owl";
+import { Component, useState, onRendered , onWillUpdateProps, onWillStart, useRef } from "@odoo/owl";
 import { TagsList } from "@web/core/tags_list/tags_list";
-import { formatMonetary } from "@web/views/fields/formatters";
+import { usePos } from "@point_of_sale/app/store/pos_hook";
+import { TextInputPopup } from "@point_of_sale/app/utils/input_popups/text_input_popup";
+import { useService } from "@web/core/utils/hooks";
+import { makeAwaitable } from "@point_of_sale/app/store/make_awaitable_dialog";
 export class CustomPaymentScreen extends Component {
     static template = "conversion.CustomPaymentScreen";
      // Definir las propiedades que recibirá el componente
@@ -11,6 +14,10 @@ export class CustomPaymentScreen extends Component {
     static components = { TagsList };
 
     setup() {
+        
+        this.pos = usePos();
+        this.dialog = useService("dialog");
+
         onRendered(() => {
            console.log('paymentScreen entrando.....')
         }); 
@@ -21,5 +28,18 @@ export class CustomPaymentScreen extends Component {
     }
     async onClick() {
         console.log("Custom button paymentScreen.");
+        const payload = await this.openTextInput('saymon and saymon');
+        console.log({payload})
+    }
+
+
+    async openTextInput(selectedNote) {
+        let buttons = [];
+        return await makeAwaitable(this.dialog, TextInputPopup, {
+            title: _t("Add %s", "Bienvenido saymons"),
+            buttons,
+            rows: 4,
+            startingValue: selectedNote,
+        });
     }
 }
