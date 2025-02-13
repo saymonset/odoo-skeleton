33101c026ed96876358546fd7098017b2887572d
saymonset <saymon_set@hotmail.com.com>
2025-02-13 15:30:37 +0000

    Se hereda los paymenLines

--- docker/18.0/addons/conversion/static/src/app/screens/product_screen/payment_screen_payment_lines/custom_payment_screen_payment_lines/custom_payment_screen_payment_lines.js
+++ docker/18.0/addons/conversion/static/src/app/screens/product_screen/payment_screen_payment_lines/custom_payment_screen_payment_lines/custom_payment_screen_payment_lines.js
@@ -3,50 +3,35 @@ import { _t } from "@web/core/l10n/translation";
 import { Component, useState, onRendered , onWillUpdateProps, onWillStart, useRef } from "@odoo/owl";
  
  
-export class CustomPaymentScreenMethods extends Component {
-    static template = "conversion.CustomPaymentScreenMethods";
+export class CustomPaymentScreenPaymentLines extends Component {
+    static template = "conversion.CustomPaymentScreenPaymentLines";
      // Definir las propiedades que recibirá el componente
      static components = {
        
     };
-    static props = {
-        //orderUuid: String,
+   // Define las propiedades que el componente espera recibir
+   static props = {
+        paymentLines: Array, // Asegúrate de que el tipo sea correcto
     };
 
     setup() {
+        
         super.setup();
+        
+          console.log("********Payment lines recibidas:", this.props.paymentLines);
 
-         // Asegurarse de que cada línea de pago tenga el campo `amount_saymon`
-        //  this.props.paymentLines.forEach((line) => {
-        //     if (!line.amount_saymon) {
-        //         line.amount_saymon = this.calculateAmountSaymon(line);
-        //     }
-        // });
-       
         onRendered(() => {
-       
         });
-        
-
-        
         onWillUpdateProps((nextProps) => {
-            console.log("Props actualizadas:", nextProps);
+            console.log("******************Props actualizadas:", nextProps);
         });  
-
     }
- 
-
     calculateAmountSaymon(line) {
         // Lógica personalizada para calcular el campo en el frontend (si es necesario)
         return line.get_amount() / 60; // Ejemplo: multiplicar por 1.5
     }
-    
     // Sobrescribe el método addNewPaymentLine
-  
     async onClick() {
-        console.log("Custom button paymentScreen.");
-    
+        console.log("Lineas...Custom button paymentScreen.");
     }
-
-
 }
